fddc5c911721c88ff7fcc860d30b33179173c803
Josué Salazaku <josuesalazaku@gmail.com>
2024-03-22 19:12:16 +0100

    Create useEffect to fetch backend data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,17 @@
-
+import { useEffect, useState } from "react"
 
 function App() {
 
+  const [backendData, setBackendData] = useState([{}]);
+  useEffect(() => {
+    fetch("/api").then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackendData(data)
+      }
+    )
+  }, [])
 
   return (
     <>
