bffcac2fb519247c5af84070c33af69b59ecf98f
Josué Salazaku <josuesalazaku@gmail.com>
2024-03-22 22:36:48 +0100

    Creae SignUp, SingIn and Explore pages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
+import SignIn from './Pages/SignIn';
+import SignUp from './Pages/SignUp';
+import Explore from './Pages/Explore';
 import './App.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -9,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Router>
       <Routes>
         <Route exact path="/" element={<App />} />
+        <Route path="/Explore" element={<Explore />} />
         <Route path="/SignUp" element={<SignIn />} />
          <Route path="/SignUp" element={<SignUp />} />
       </Routes>
