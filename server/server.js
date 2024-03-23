5aacefe75fe63ad4c3195fe1e9d664a7be39cee3
Josué Salazaku <josuesalazaku@gmail.com>
2024-03-23 14:44:06 +0100

    Create server post routes

--- server/server.js
+++ server/server.js
@@ -1,43 +1,30 @@
-import express from 'express';
-import cors from 'cors';
-import multer from 'multer';
-import mysql from 'mysql2';
-import dotenv from 'dotenv';
+import express from "express";
+import cors from "cors";
+import multer from "multer"
+
+import postRoutes from "/Users/josuesalazaku/Developer/Projects/Foodie-travellers/server/routes/posts.js"
+
+import dotenv from "dotenv";
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
-  
-db.connect((err) => {
-    if (err) {
-      console.error('Error connecting to MySQL database:', err);
-      return;
-    }
-    console.log('Connected to MySQL database');
-  });
-
 app.use(cors());
-const upload = multer({ dest: 'uploads/' });
+const upload = multer({ dest: "uploads/" });
+
+app.use(express.json());
 
 // Handle POST requests to '/api/posts'
-app.post('/api/posts', upload.single('image'), (req, res) => {
-    const { title, content } = req.body;
-    const imagePath = req.file.path;
-  });
-  
+app.post("/api/posts", upload.single("image"), (req, res) => {
+  const { title, content } = req.body;
+  const imagePath = req.file.path;
+});
 
-app.get("/api/post", (req, res) => {
-
+app.get("/test/", (req, res) => {
+  res.json("it works?");
 });
 
 app.listen(port, () => {
-    console.log(`Server running on ${port}`);
+  console.log(`Server running on ${port}`);
 });
