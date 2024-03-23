90415401c239849f89235de5af3de10be9a46a08
Josué Salazaku <josuesalazaku@gmail.com>
2024-03-23 13:37:03 +0100

    Creat Login form

--- client/src/Pages/LogIn.jsx
+++ client/src/Pages/LogIn.jsx
@@ -1,8 +1,22 @@
-
 function SignIn() {
   return (
-    <div>SignIn</div>
-  )
+    <main className="h-screen flex flex-col justify-start items-center gap-20">
+      <h1 className="text-6xl">Welcome Back Traveller</h1>
+      <form action="" className="border flex flex-col h-96 w-96 ">
+        <div className="gap-5 flex flex-col justify-center mb-5">
+          <input
+            type="text"
+            className="border h-9 rounded-lg p-2 border-purple-700"
+          />
+          <input
+            type="text"
+            className="border h-9 rounded-lg p-2 border-purple-700"
+          />
+        </div>
+        <button className="border border-purple-700">Log In </button>
+      </form>
+    </main>
+  );
 }
 
-export default SignIn
+export default SignIn;
