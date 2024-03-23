66ff2ec60220b4578e39f8a130fc1f162408a8c0
Josué Salazaku <josuesalazaku@gmail.com>
2024-03-23 15:03:45 +0100

    Create post routes in auth for login logout and SingUp

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,11 +1,11 @@
 import express from "express";
-
-
+import { logIn, logOut, singUp } from "../Controllers/auth.js";
 
 const router = express.Router();
 
-
 //posts route
-router.post("/SignUp"); 
+router.post("/SignUp", singUp);
+router.post("/SignUp", logIn);
+router.post("/SignUp", logOut);
 
 export default router;
