46b5af6fd1a3fbfcb0252989e88c66b82ec73e0d
Josué Salazaku <josuesalazaku@gmail.com>
2024-03-23 15:58:12 +0100

    Hash users passwords with bcrypt

--- server/Controllers/auth.js
+++ server/Controllers/auth.js
@@ -1,11 +1,20 @@
 import db from '../db.js';
+import bcrypt from 'bcryptjs'
 
 
 export const singUp = (res, req) => {
     // Check existing user 
     const q = "SELECT * FROM users WHERE email = ? OR username = ?"
 
-    db.query(q, [req.body.email, req.body.name])
+    db.query(q, [req.body.email, req.body.name], (err, data) => {
+        if (err) return res.json(err)
+        if (data.length) return res.status(409).json("User already exists!")
+
+        //Hash the passwords and create a user
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(req.body.password, salt);
+
+    })
 }
 
 export const logIn = (res, req) => {
