8faa47cc322260951ab35b10c32dcee4ad5b33f3
Josué Salazaku <josuesalazaku@gmail.com>
2024-03-23 13:53:36 +0100

    Improve Header component

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -3,11 +3,13 @@ import { Link } from 'react-router-dom';
 function Header() {
   return (
     <header className="w-screen h-20 flex flex-row">
-      <ul className="w-screen flex flex-row justify-evenly items-center font-extrabold">
+      <ul className="w-screen flex flex-row justify-around items-center font-extrabold">
         <li><Link to="/">Logo</Link></li>
         <li><Link to="/Explore">Explore</Link></li>
+        <div className='flex flex-row gap-5'>
         <li><Link to="/LogIn">Log In</Link></li>
         <li><Link to="/SignUp">Sign Up</Link></li>
+        </div>
       </ul>
     </header>
   );
--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -3,15 +3,15 @@ import {Link} from 'react-router-dom'
 function SingUp() {
   return (
     <main className="h-screen flex flex-col justify-start items-center gap-20">
-      <h1 className="text-6xl">Welcome Back Traveller</h1>
+      <h1 className="text-6xl">Welcome New Foodie Traveller</h1>
       <form action="" className="border flex flex-col h-96 w-96 ">
         <div className="gap-5 flex flex-col justify-center mb-5">
           <input
-            type="text" placeholder='First Name'
+            type="text" placeholder='Username'
             className="border h-9 rounded-lg p-2 border-purple-700"
           />
           <input
-            type="text" placeholder='Laste Name'
+            type="text" placeholder='Email'
             className="border h-9 rounded-lg p-2 border-purple-700"
           />
             <input
@@ -21,7 +21,7 @@ function SingUp() {
         </div>
         <button className="border border-purple-700">Sign up</button>
       </form>
-      <span>Don't have an account? <Link to="/SignUp" className="text-blue-800 font-extrabold">Sign up!</Link></span>
+      <span>Do you have an account? <Link to="/Login" className="text-blue-800 font-extrabold">Log in!</Link></span>
     </main>
   );
 }
