3b4dafa363c4e704ac4da392298118129edd4e6b
Josué Salazaku <josuesalazaku@gmail.com>
2024-03-30 13:56:31 +0100

    Improve post route

--- server/Controllers/posts.js
+++ server/Controllers/posts.js
@@ -1,8 +1,3 @@
-
-
-
-
-// All crud operations or haneling for posts happen inside this conrtroller
 export const addPost = (req, res) => {
     res.json("From controller")
 }
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -7,5 +7,10 @@ const router = express.Router();
 
 //posts route
 router.get("/", addPost); 
+router.get("/:id", addPost); 
+router.post("/", addPost); 
+router.delete("/", addPost); 
+//this is Update by id
+router.put("/:id",); 
 
 export default router;
