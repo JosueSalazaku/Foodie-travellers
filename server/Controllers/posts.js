b0e24bc1e406d36f53dfaf182f27aca726e5cf0f
Josué Salazaku <josuesalazaku@gmail.com>
2024-03-30 14:25:15 +0100

    Add query to get posts function

--- server/Controllers/posts.js
+++ server/Controllers/posts.js
@@ -1,5 +1,13 @@
+import db from "../db";
+
 export const getPosts = (req, res) => {
-    res.json("From controller")
+    const q = res.query.category ? "SELECT * FROM posts WHERE category=?" :
+        "SELECT * FROM posts";
+    db.query(q[req.query.category], (err, data) => {
+        if (err) return res.send(err);
+
+        return res.status(200).json(data);
+    })
 }
 
 export const getPost = (req, res) => {
