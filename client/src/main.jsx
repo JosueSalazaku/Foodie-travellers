abf92f6f473f9ea2d3acd57f520872e7ba0202c6
Josué Salazaku <josuesalazaku@gmail.com>
2024-04-02 16:56:47 +0200

    Add settings to dropdown menu

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -14,6 +14,7 @@ import AuthContextProvider from './Context/authContext';
 import Profile from './Pages/Profile';
 import Write from './Pages/Write';
 import Library from './Pages/Library';
+import Settings from './Pages/Settings';
 
 const root = ReactDOM.createRoot(document.getElementById('root')); 
 root.render(
@@ -28,7 +29,8 @@ root.render(
           <Route path="/Explore" element={<Explore />} />
           <Route path="/Write" element={<Write />} />
           <Route path="/LogIn" element={<LogIn />} />
-          <Route path="/Library" element={<Library />} />
+            <Route path="/Library" element={<Library />} />
+            <Route path="/Settings" element={<Settings />} />
           <Route path="/SignUp" element={<SignUp />} />
           <Route path="/Profile" element={<Profile/>}/>
           <Route path="/LogOut" element={<LogOut />} />
